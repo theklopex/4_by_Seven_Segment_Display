61412221e1646006d21e0afdc594cf6a00c41413
captain <klopex@klopex.com>
2019-01-06 15:56:23 -0500

    Supports printing integers, floats, and timer countdowns.

--- Four_by_SevenSegmentDisplay.ino
+++ Four_by_SevenSegmentDisplay.ino
@@ -1,4 +1,9 @@
 // Public Domain
+//
+// Use at your own risk!
+//
+// Have a nice day!
+
 // whatwilldadmando.wordpress.com
 
 // Counts down on a four-digit seven-segment display.
@@ -20,6 +25,9 @@ int d3 = 10;
 int d2 = 11;
 int d1 = 12;
 
+float startFloatNum = 120.0;
+unsigned int startIntNum = 10000;
+unsigned long startTime;
 
 void setup() {
   pinMode(d1, OUTPUT);
@@ -34,10 +42,18 @@ void setup() {
   pinMode(f, OUTPUT);
   pinMode(g, OUTPUT);
   pinMode(dp, OUTPUT);
+
+  startTime = millis();
 }
 
-void showZero()
+void showZero(bool showDecimal)
 {
+  // Default is to have decimal off
+  int decimalValue = HIGH;
+  if (showDecimal)
+  {
+    decimalValue = LOW;
+  }
   digitalWrite(a, LOW);
   digitalWrite(b, LOW);
   digitalWrite(c, LOW);
@@ -45,11 +61,17 @@ void showZero()
   digitalWrite(e, LOW);
   digitalWrite(f, LOW);
   digitalWrite(g, HIGH);
-  digitalWrite(dp, HIGH);
+  digitalWrite(dp, decimalValue);
 }
 
-void showOne()
+void showOne(bool showDecimal)
 {
+  // Default is to have decimal off
+  int decimalValue = HIGH;
+  if (showDecimal)
+  {
+    decimalValue = LOW;
+  }
   digitalWrite(a, HIGH);
   digitalWrite(b, LOW);
   digitalWrite(c, LOW);
@@ -57,11 +79,17 @@ void showOne()
   digitalWrite(e, HIGH);
   digitalWrite(f, HIGH);
   digitalWrite(g, HIGH);
-  digitalWrite(dp, HIGH);
+  digitalWrite(dp, decimalValue);
 }
 
-void showTwo()
+void showTwo(bool showDecimal)
 {
+  // Default is to have decimal off
+  int decimalValue = HIGH;
+  if (showDecimal)
+  {
+    decimalValue = LOW;
+  }
   digitalWrite(a, LOW);
   digitalWrite(b, LOW);
   digitalWrite(c, HIGH);
@@ -69,11 +97,17 @@ void showTwo()
   digitalWrite(e, LOW);
   digitalWrite(f, HIGH);
   digitalWrite(g, LOW);
-  digitalWrite(dp, HIGH);
+  digitalWrite(dp, decimalValue);
 }
 
-void showThree()
+void showThree(bool showDecimal)
 {
+  // Default is to have decimal off
+  int decimalValue = HIGH;
+  if (showDecimal)
+  {
+    decimalValue = LOW;
+  }
   digitalWrite(a, LOW);
   digitalWrite(b, LOW);
   digitalWrite(c, LOW);
@@ -81,11 +115,17 @@ void showThree()
   digitalWrite(e, HIGH);
   digitalWrite(f, HIGH);
   digitalWrite(g, LOW);
-  digitalWrite(dp, HIGH);
+  digitalWrite(dp, decimalValue);
 }
 
-void showFour()
+void showFour(bool showDecimal)
 {
+  // Default is to have decimal off
+  int decimalValue = HIGH;
+  if (showDecimal)
+  {
+    decimalValue = LOW;
+  }
   digitalWrite(a, HIGH);
   digitalWrite(b, LOW);
   digitalWrite(c, LOW);
@@ -93,11 +133,17 @@ void showFour()
   digitalWrite(e, HIGH);
   digitalWrite(f, LOW);
   digitalWrite(g, LOW);
-  digitalWrite(dp, HIGH);
+  digitalWrite(dp, decimalValue);
 }
 
-void showFive()
+void showFive(bool showDecimal)
 {
+  // Default is to have decimal off
+  int decimalValue = HIGH;
+  if (showDecimal)
+  {
+    decimalValue = LOW;
+  }
   digitalWrite(a, LOW);
   digitalWrite(b, HIGH);
   digitalWrite(c, LOW);
@@ -105,11 +151,17 @@ void showFive()
   digitalWrite(e, HIGH);
   digitalWrite(f, LOW);
   digitalWrite(g, LOW);
-  digitalWrite(dp, HIGH);
+  digitalWrite(dp, decimalValue);
 }
 
-void showSix()
+void showSix(bool showDecimal)
 {
+  // Default is to have decimal off
+  int decimalValue = HIGH;
+  if (showDecimal)
+  {
+    decimalValue = LOW;
+  }
   digitalWrite(a, LOW);
   digitalWrite(b, HIGH);
   digitalWrite(c, LOW);
@@ -117,11 +169,17 @@ void showSix()
   digitalWrite(e, LOW);
   digitalWrite(f, LOW);
   digitalWrite(g, LOW);
-  digitalWrite(dp, HIGH);
+  digitalWrite(dp, decimalValue);
 }
 
-void showSeven()
+void showSeven(bool showDecimal)
 {
+  // Default is to have decimal off
+  int decimalValue = HIGH;
+  if (showDecimal)
+  {
+    decimalValue = LOW;
+  }
   digitalWrite(a, LOW);
   digitalWrite(b, LOW);
   digitalWrite(c, LOW);
@@ -129,11 +187,17 @@ void showSeven()
   digitalWrite(e, HIGH);
   digitalWrite(f, HIGH);
   digitalWrite(g, HIGH);
-  digitalWrite(dp, HIGH);
+  digitalWrite(dp, decimalValue);
 }
 
-void showEight()
+void showEight(bool showDecimal)
 {
+  // Default is to have decimal off
+  int decimalValue = HIGH;
+  if (showDecimal)
+  {
+    decimalValue = LOW;
+  }
   digitalWrite(a, LOW);
   digitalWrite(b, LOW);
   digitalWrite(c, LOW);
@@ -141,11 +205,17 @@ void showEight()
   digitalWrite(e, LOW);
   digitalWrite(f, LOW);
   digitalWrite(g, LOW);
-  digitalWrite(dp, HIGH);
+  digitalWrite(dp, decimalValue);
 }
 
-void showNine()
+void showNine(bool showDecimal)
 {
+  // Default is to have decimal off
+  int decimalValue = HIGH;
+  if (showDecimal)
+  {
+    decimalValue = LOW;
+  }
   digitalWrite(a, LOW);
   digitalWrite(b, LOW);
   digitalWrite(c, LOW);
@@ -153,7 +223,7 @@ void showNine()
   digitalWrite(e, HIGH);
   digitalWrite(f, LOW);
   digitalWrite(g, LOW);
-  digitalWrite(dp, HIGH);
+  digitalWrite(dp, decimalValue);
 }
 
 void clearLEDs()
@@ -168,25 +238,42 @@ void clearLEDs()
   digitalWrite(dp, HIGH);
 }
 
-unsigned int myNumber = 10000;
 int stallTime = 0;
 
 // the loop function runs over and over again forever
 void loop() {
-  unsigned int tempNumber = myNumber;
-  unsigned char ones;
-  unsigned char tens;
-  unsigned char hundreds;
-  unsigned char thousands;
+  //countDownFloat();
+  //countDownInt();
+  timerCountdown(startFloatNum);
+}
 
-  ones = tempNumber % 10;
-  tempNumber = tempNumber / 10;
-  tens = tempNumber % 10;
-  tempNumber = tempNumber / 10;
-  hundreds = tempNumber % 10;
-  tempNumber = tempNumber / 10;
-  thousands = tempNumber % 10;
+float timeRemaining = 0xffff;
+void timerCountdown(float numSeconds)
+{
+  unsigned long nowTime;
+  float endTime;
+  float floatSeconds;
 
+  endTime = startTime + (numSeconds * 1000.0);
+
+  nowTime = millis();
+  if (timeRemaining > 0)
+  {
+    timeRemaining = endTime - nowTime;
+  }
+  else
+  {
+    timeRemaining = 0;
+  }
+  floatSeconds = ((float)timeRemaining) / 1000.0;
+
+//  displayInt(int(timeRemaining), 0);
+  displayFloat(floatSeconds);
+}
+
+float myIntNumber = startIntNum;
+void countDownInt()
+{
   if (stallTime < 20)
   {
     stallTime++;
@@ -194,19 +281,38 @@ void loop() {
   }
   else
   {
-    myNumber -= 1;
+    myIntNumber -= 1;
     stallTime = 0;
   }
 
-  displayPos(1,thousands);
-  displayPos(2,hundreds);
-  displayPos(3,tens);
-  displayPos(4,ones);
+  displayInt(myIntNumber, 0);
 
-  if (myNumber == 0)
+  if (myIntNumber < 0)
   {
-    myNumber = 10000;
+    myIntNumber = startIntNum;
+  }    
+}
+
+float myFloatNumber = startFloatNum;
+void countDownFloat()
+{
+  if (stallTime < 20)
+  {
+    stallTime++;
+    delay(1);
   }
+  else
+  {
+    myFloatNumber -= .1;
+    stallTime = 0;
+  }
+
+  displayFloat(myFloatNumber);
+
+  if (myFloatNumber < 0.001)
+  {
+    myFloatNumber = startFloatNum;
+  }  
 }
 
 void port(unsigned char n)
@@ -246,39 +352,39 @@ void port(unsigned char n)
   }
 }
 
-void pickNumber(unsigned char n)
+void pickNumber(unsigned char n, bool showDecimal)
 {
   switch(n)
   {
     case 0:
-      showZero();
+      showZero(showDecimal);
       break;
     case 1:
-      showOne();
+      showOne(showDecimal);
       break;
     case 2:
-      showTwo();
+      showTwo(showDecimal);
       break;
     case 3:
-      showThree();
+      showThree(showDecimal);
       break;
     case 4:
-      showFour();
+      showFour(showDecimal);
       break;
     case 5:
-      showFive();
+      showFive(showDecimal);
       break;
     case 6:
-      showSix();
+      showSix(showDecimal);
       break;
     case 7:
-      showSeven();
+      showSeven(showDecimal);
       break;
     case 8:
-      showEight();
+      showEight(showDecimal);
       break;
     case 9:
-      showNine();
+      showNine(showDecimal);
       break;
     default:
       clearLEDs();
@@ -286,10 +392,70 @@ void pickNumber(unsigned char n)
   }
 }
 
-void displayPos(unsigned char x, unsigned char number)
+void displayFloat(float number)
+{
+  // Determine where to place the decimal.
+  int intNumber = int(number);
+  unsigned char significantDigits;
+  unsigned char decimalPosition;
+
+  // Remove one order of magnitude by dividing by zero and starting with significantDigits=1
+  // This will guarantee that it will end with 0.000 instead of .0000 (which will not be able to display the decimal point.  It will be pretty!
+  intNumber = intNumber / 10;
+  for (significantDigits=1; significantDigits<5; significantDigits++)
+  {
+    if (intNumber > 0)
+    {
+      intNumber = intNumber / 10;
+    }
+    else
+    {
+      break;
+    }
+  }
+  decimalPosition = 4 - significantDigits;
+
+  for (int i = decimalPosition; i > 0; i--)
+  {
+    number = number * 10;
+  }
+
+  displayInt(int(number), decimalPosition);
+}
+
+void displayInt(unsigned int number, unsigned char decimalPosition)
+{
+  // The four positions are p4, p3, p2, p1
+  // decimal position:
+  //   p1 = 0
+  //   p2 = 1
+  //   p3 = 2
+  //   p4 = 3
+  //   nowhere = other
+
+  unsigned char ones;
+  unsigned char tens;
+  unsigned char hundreds;
+  unsigned char thousands;
+
+  ones = number % 10;
+  number = number / 10;
+  tens = number % 10;
+  number = number / 10;
+  hundreds = number % 10;
+  number = number / 10;
+  thousands = number % 10;
+  
+  displayPos(1, thousands, decimalPosition==3);
+  displayPos(2, hundreds, decimalPosition==2);
+  displayPos(3, tens, decimalPosition==1);
+  displayPos(4, ones, decimalPosition==0);
+}
+
+void displayPos(unsigned char x, unsigned char number, bool showDecimal)
 {
   port(x);
-  pickNumber(number);
+  pickNumber(number, showDecimal);
   delay(1);
   clearLEDs();
 }
